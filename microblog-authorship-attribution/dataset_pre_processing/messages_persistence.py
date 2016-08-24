e7983b1b07a75e5545c64677b4f909d86d35c603
Antonio Theophilo <antonio.theophilo@gmail.com>
2016-08-24 14:41:34 -0300

    Addition of messages_persistence.py file.

---
+++ microblog-authorship-attribution/dataset_pre_processing/messages_persistence.py
@@ -0,0 +1,35 @@
+"""
+Auxiliary code for manipulating (read and write) Twitter messages in dataset files.
+"""
+    
+import codecs
+
+
+def read(filename):
+    messages = []
+    str_buffer = u''
+    with codecs.open(filename, encoding='utf-8', mode='r', buffering=1, errors='strict') as fd:
+        read = False
+        for line in fd:
+            if not read:
+                if line[len(line)-2] == u'{':
+                    read = True
+            else:
+                if line == u'}\n':
+                    read = False
+                    parts = str_buffer.split('#POS')
+                    message = {}
+                    message['tweet'] = parts[0][:-1]        # strips the last \n character
+                    message['pos'] = parts[1] if len(parts) > 1 else None
+                    message['full'] = str_buffer[:-1]       # strips the last \n character
+                    messages.append(message)
+                    str_buffer = u''
+                else:
+                    str_buffer += line
+    return messages
+
+
+def write(messages, id, filename):
+    with codecs.open(filename, encoding='utf-8', mode='w+', errors='strict') as fd:
+        for message in messages:
+            fd.write(u'\n'.join([u'{', message[id], u'}\n']))
