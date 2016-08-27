7e06e135641c6fe8cc437501acad19f97d092d52
Antonio Theophilo <antonio.theophilo@gmail.com>
2016-08-26 23:47:17 -0300

    Initial version.

---
+++ microblog_authorship_attribution/utils/install_libraries.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+function notify_error {
+    echo "ERROR EXECUTING COMMAND"
+    cd $INITIAL_DIR
+    exit 1
+}
+
+
+trap notify_error ERR
+
+
+apt-get update
+apt-get --yes upgrade
+
+apt-get --yes install sudo
+
+apt-get --yes install vim
+apt-get --yes install man
+apt-get --yes install htop
+
+apt-get --yes install python-numpy
+apt-get --yes install python-scipy
+apt-get --yes install python-pip
+pip install --upgrade pip
+pip install --upgrade nltk
+pip install --upgrade scikit-learn
+pip install --upgrade ipython
+
